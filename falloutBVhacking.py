a84cc4289a88bf885a4f083a0376518b6025e9a2
Conflictious <drtrunks@gmail.com>
2017-06-10 23:39:25 +0200

    Add files via upload

---
+++ falloutBVhacking.py
@@ -0,0 +1,29 @@
+def FVin():
+    print('what are the words? seperate with space')
+    words = input().split(' ')
+    return words
+
+def pickword(words, pickedword, pickedfam):
+    possiblewords = []
+    for word in words:
+        matching = [letter for letter in pickedword if letter in word]
+        if len(matching) == pickedfam:
+            possiblewords.append(word)
+    return possiblewords
+
+def mainthing():
+    words = FVin()
+    while len(words) > 1:
+        print('pick a word:')
+        pickedword = input()
+        print('how many points did the word score? for 2/5, type 2')
+        pickedfam = int(input())
+        while pickedfam < 0 or pickedfam > 20:
+            print('incorrect number')
+            pickedfam = input()
+        words = pickword(words, pickedword, pickedfam)
+        print(" ".join(words))
+    print("done")
+
+mainthing()
+
